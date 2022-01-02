ad79b1b1f71622ed22faecb68cde575f8091e25b
zhenxiao-yu <markyu0615@gmail.com>
2022-01-02 17:47:24 -0500

    project update

--- src/components/WorkData.js
+++ src/components/WorkData.js
@@ -1,49 +1,49 @@
 export const Work = [
     {
         id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
+        name:"Spring Boot Wiki",
+        description:"Fullstack (Spring Boot + Vue 3) Wiki Application. Complete with a backstage management system for documents/ accounts, JWT authentication and E-charts for displaying trends.",
+        tags:["mysql","spring-boot","vue","java","typescript","jwt","e-charts","my-batis"],
         
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        demo:"",
+        github:"https://github.com/zhenxiao-yu/Spring-Boot-Wiki"
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
+        name:"Vue Grocery Delivery App",
+        description:"A lightweight web application that allow students to browse, save and order daily necessities from their local grocery stores.",
+        tags:["vue","sass","api","vue-cli","vue-router"],
         
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
+        demo:"",
+        github:"https://github.com/zhenxiao-yu/Vue-Grocery-Delivery-App"
         
     },
     {
         id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
+        name:"Spring Boot Blog",
+        description:"Fullstack blog website created using MySQL, spring-boot, thymeleaf and semantic UI. Posts are sorted by category, hashtags and publication dates. ",
+        tags:["javascript","css","jquery","thymeleaf","spring-boot","html","semantic-ui"],
         
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
+        demo:"",
+        github:"https://github.com/zhenxiao-yu/Spring-Boot-Blog"
     },
     {
         id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
+        name:"React E-Commerce Website",
+        description:"Online Store created using React.Js, JSON Server (Backend API), Node SASS, and CSS Framework (Bulma). Designed for small businesses that are looking to sell their products online.",
+        tags:["react","json","react-router","json-api","custom-hooks","bulma","sass"],
         
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
+        demo:"",
+        github:"https://github.com/zhenxiao-yu/React-Ecommerce-Website"
        
     },{
         id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
+        name:"Web3 Crypto Wallet",
+        description:"Decentralized web 3.0 application that helps user send crypto across the world using blockchain smart contracts",
+        tags:["react","solidity","tailwindcss","blockchain","javascript"],
         
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        demo:"",
+        github:"https://github.com/zhenxiao-yu/web3-blockchain-app"
         
     },
     {
