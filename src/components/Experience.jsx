e9dcf409413864a84dec45f775fda0bb764d581c
Mark Yu <zyu347@uwo.ca>
2024-04-10 23:50:17 -0400

    placed avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+      <Avatar/>
       <mesh>
         <boxGeometry />
         <meshNormalMaterial />
