3fd4d4537485f2422842d7457d170f121c081c31
Mark Yu <zyu347@uwo.ca>
2024-04-11 02:15:54 -0400

    Revert "added new lib"
    This reverts commit 254e346c8c2367ffb4981edccaa81cc44a6ca6b4.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,18 @@
 import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
 
 export const Experience = () => {
   return (
-    <>
+    <Canvas
+      style={{ background: "#D2B48C" }} // Tan color background
+      camera={{ position: [0, 0, 5] }} // Set camera position
+    >
       <OrbitControls />
-      <group position-y={-1.5}>
+      <group position-y={-1}>
          <Avatar />
       </group>
       <ambientLight intensity={0.85}/>
-    </>
+    </Canvas>
   );
 };
