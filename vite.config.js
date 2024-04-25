d8c65097357535fa5518f67524fd168f151db0dd
Mark Yu <zyu347@uwo.ca>
2024-04-25 15:56:57 -0400

    converted top use vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),  // ensures all necessary React and JSX processing
+        viteTsconfigPaths(),
+        svgrPlugin()
+    ],
+    server: {
+        open: true,
+        port: 3000,
+    },
+});
