2f2d5511349a3ef4468b3850522c92bbedba6718
Mark Yu <zyu347@uwo.ca>
2024-05-03 01:17:15 -0400

    Update BlogData.js

--- src/assets/data/BlogData.js
+++ src/assets/data/BlogData.js
@@ -1,19 +1,19 @@
 export const Blogs = [
     {
         id:1,
-        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
-        tags:["react","gsap","styled-components"],
-        date:"13 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
+        name:"Demystifying Heuristic Search Algorithms",
+        tags:["AI","Algorithms","Heuristics","A*"],
+        date:"02 May, 2024",
+        imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F4plybk2sh2h1x4irffxq.jpg",
+        link:"https://dev.to/markyu/demystifying-heuristic-search-algorithms-59lh"
     },
     {
         id:2,
-        name:"How to create React Hamburger Menu using Styled-Components",
-        tags:["react","styled-components"],
-        date:"5 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
+        name:"3 Ways To Create Engaging React Loading Screens with Hooks",
+        tags:["react","hooks","frontend","loader"],
+        date:"01 May, 2024",
+        imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fjyg48gpypcar4zoaam0q.jpg",
+        link:"https://dev.to/markyu/3-ways-to-create-engaging-react-loading-screens-with-hooks-37b1"
     },
     {
         id:3,
