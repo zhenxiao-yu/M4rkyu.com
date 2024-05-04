3d99752615855ad7763979572a953fa56b8d1a94
Mark Yu <zyu347@uwo.ca>
2024-05-03 23:31:02 -0400

    Update BlogData.js

--- src/assets/data/BlogData.js
+++ src/assets/data/BlogData.js
@@ -11,18 +11,49 @@ export const Blogs = [
         id:2,
         name:"3 Ways To Create Engaging React Loading Screens with Hooks",
         tags:["react","hooks","frontend","loader"],
-        date:"01 May, 2024",
+        date:"04 April, 2024",
         imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fjyg48gpypcar4zoaam0q.jpg",
         link:"https://dev.to/markyu/3-ways-to-create-engaging-react-loading-screens-with-hooks-37b1"
     },
     {
         id:3,
-        name:"Introduction to Docker Containers [With Commands]",
+        name:"Introduction to Docker Containers 🐋📦 [With Commands]",
         tags:["cloudcomputing","docker","virtualmachine"],
-        date:"6 July, 2021",
+        date:"05 May, 2024",
         imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Femf2i4zub5l554rewwdb.jpg",
         link:"https://dev.to/markyu/introduction-to-docker-containers-with-commands-amf"
     },
-    
+    {
+        id:4,
+        name:"Tips for Implementing Sustainable Cloud Design",
+        tags:["cloudcomputing","datacenter","greencomputing", "sustainability"],
+        date:"06 April, 2024",
+        imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F52s7hz48pc1itqy42fdl.png",
+        link:"https://dev.to/markyu/tips-for-implementing-sustainable-cloud-design-1l4g"
+    },
+    {
+        id:5,
+        name:"Encrypting with Block Ciphers: A Guide to AES, CBC, and More",
+        tags:["security","cryptography","blockcipher","encryption"],
+        date:"19 April, 2024",
+        imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fi4x5yt6x3w13jzxz8fxl.png",
+        link:"https://dev.to/markyu/encrypting-with-block-ciphers-a-guide-to-aes-cbc-and-more-31gn"
+    },
+    {
+        id:6,
+        name:"Mastering Kubernetes: A Guide to Container Orchestration",
+        tags:["devops","kubernetes","architecture","container"],
+        date:"22 February, 2024",
+        imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fsa2pow1q2tmgy37vkf6d.jpg",
+        link:"https://dev.to/markyu/mastering-kubernetes-a-guide-to-container-orchestration-37fe"
+    },
+    {
+        id:7,
+        name:"👀Top 10 Must-Try VS Code Themes in 2024",
+        tags:["ui","vscode","themes","recommendation"],
+        date:"22 January, 2024",
+        imgSrc:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F6ppuvkisgdg7a9ievrwb.png",
+        link:"https://dev.to/markyu/top-10-must-try-vs-code-themes-in-2024-2efj"
+    },
   
 ] 
