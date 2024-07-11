115a10a911832d7146e71b3e2fe2e4a6fa17ff97
Mark Yu <zyu347@uwo.ca>
2024-07-11 14:12:13 -0400

    impoved blog page

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -23,19 +23,21 @@ const Box = styled(motion(NavLink))`
     color: ${(props) => props.theme.body};
     background-color: ${(props) => props.theme.text};
     transition: all 0.3s ease;
+    box-shadow: 0 0.5rem 1rem 0 rgba(8, 9, 10, 0.8);
   }
 
   ${mediaQueries(50)`
-    width: calc(58vw);
+    width: calc(50vw + 6rem);
   `};
 
   ${mediaQueries(30)`
     height: 18rem;
+    padding: 0.8rem;
   `};
 
   ${mediaQueries(25)`
-    height: 14rem;
-    padding: 0.8rem;
+    height: 16rem;
+    padding: 0.6rem;
     backdrop-filter: none;
   `};
 `;
@@ -43,10 +45,10 @@ const Box = styled(motion(NavLink))`
 const Image = styled.div`
   background-image: ${(props) => `url(${props.img})`};
   width: 100%;
-  height: 100%;
+  height: 80%;
   border-radius: 0.5rem;
   background-size: cover;
-  border: 1px solid transparent;
+  border: 2px solid rgb(8, 9, 10);
   background-position: center center;
 
   ${mediaQueries(50)`
@@ -54,7 +56,7 @@ const Image = styled.div`
   `};
 
   ${mediaQueries(25)`
-    height: 70%;
+    height: 50%;
   `};
 
   ${Box}:hover & {
@@ -75,10 +77,20 @@ const Title = styled.h3`
 
   ${mediaQueries(40)`
     font-size: calc(0.8em + 1vw);
+    white-space: normal;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
   `};
 
   ${mediaQueries(25)`
     font-size: calc(0.6em + 1vw);
+    white-space: normal;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
   `};
 
   border-bottom: 2px solid ${(props) => props.theme.text};
@@ -90,10 +102,12 @@ const Title = styled.h3`
 
 const HashTags = styled.div`
   padding: 0.5rem 0;
-  
   max-height: 3rem;
   overflow: hidden;
   text-overflow: ellipsis;
+  display: flex;
+  flex-wrap: wrap;
+
   ${mediaQueries(25)`
     font-size: calc(0.5em + 1vw);
   `};
@@ -106,7 +120,7 @@ const Tag = styled.span`
   text-overflow: ellipsis;
   font-family: karla, sans-serif;
   font-weight: 700;
-  
+
   ${mediaQueries(40)`
     font-size: calc(0.7em + 1vw);
   `};
@@ -119,21 +133,28 @@ const Tag = styled.span`
 const Date = styled.span`
   padding: 0.5rem 0;
   font-weight: 500;
+
   ${mediaQueries(25)`
     font-size: calc(0.7em + 1vw);
   `};
 `;
 
 const Container = styled(motion.div)``;
+
 const item = {
-  hidden: { scale: 0 },
-  show: { scale: 1, transition: { type: "spring", duration: 0.5 } },
+  hidden: { scale: 0, opacity: 0 },
+  show: { scale: 1, opacity: 1, transition: { type: "spring", duration: 0.5 } },
 };
 
 const BlogComponent = (props) => {
   const { name, tags, date, imgSrc, link } = props.blog;
   return (
-    <Container variants={item}>
+    <Container
+      initial="hidden"
+      animate="show"
+      whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+      variants={item}
+    >
       <Box target="_blank" to={{ pathname: `${link}` }}>
         <Image className="animate__animated animate__bounceIn animate__delay-1s" img={imgSrc} />
         <Title>{name}</Title>
@@ -142,7 +163,9 @@ const BlogComponent = (props) => {
             <Tag key={id}>#{t}</Tag>
           ))}
         </HashTags>
-        <Date><MdDateRange size="0.95em" /> {date}</Date>
+        <Date>
+          <MdDateRange size="0.95em" /> {date}
+        </Date>
       </Box>
     </Container>
   );
