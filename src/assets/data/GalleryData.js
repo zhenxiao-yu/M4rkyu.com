ed5b1477bf428f3d37b11c9bee4d63aec9611819
Mark Yu <zyu347@uwo.ca>
2024-07-11 21:07:15 -0400

    sped up animation

---
+++ src/assets/data/GalleryData.js
@@ -0,0 +1,68 @@
+// src/data/galleryData.js
+import Img1 from '../../assets/gallery/img1.jpg';
+import Img2 from '../../assets/gallery/img2.jpg';
+import Img3 from '../../assets/gallery/img3.jpg';
+import Img4 from '../../assets/gallery/img4.jpg';
+import Img5 from '../../assets/gallery/img5.jpg';
+import Img6 from '../../assets/gallery/img6.jpg';
+import Img7 from '../../assets/gallery/img7.jpg';
+import Img8 from '../../assets/gallery/img8.jpg';
+import Img9 from '../../assets/gallery/img9.jpg';
+import Img10 from '../../assets/gallery/img10.jpg';
+import Img11 from '../../assets/gallery/img11.jpg';
+
+const data = [
+  { id: 1, imgSrc: Img1, title: 'Nature 1', subtitle: 'Subtitle 1', date: '' },
+  { id: 2, imgSrc: Img2, title: 'Nature 2', subtitle: 'Subtitle 2', date: '' },
+  { id: 3, imgSrc: Img3, title: 'Nature 3', subtitle: 'Subtitle 3', date: '' },
+  { id: 4, imgSrc: Img4, title: 'Nature 4', subtitle: 'Subtitle 4', date: '' },
+  { id: 5, imgSrc: Img5, title: 'Nature 5', subtitle: 'Subtitle 5', date: '' },
+  { id: 6, imgSrc: Img6, title: 'Architecture 1', subtitle: 'Subtitle 6', date: '' },
+  { id: 7, imgSrc: Img7, title: 'Architecture 2', subtitle: 'Subtitle 7', date: '' },
+  { id: 8, imgSrc: Img8, title: 'Architecture 3', subtitle: 'Subtitle 8', date: '' },
+  { id: 9, imgSrc: Img9, title: 'Architecture 4', subtitle: 'Subtitle 9', date: '' },
+  { id: 10, imgSrc: Img10, title: 'Architecture 5', subtitle: 'Subtitle 10', date: '' },
+  { id: 11, imgSrc: Img11, title: 'Urban Life 1', subtitle: 'Subtitle 11', date: '' },
+  { id: 12, imgSrc: Img4, title: 'Urban Life 2', subtitle: 'Subtitle 12', date: '' },
+  { id: 13, imgSrc: Img5, title: 'Urban Life 3', subtitle: 'Subtitle 13', date: '' },
+  { id: 14, imgSrc: Img6, title: 'Urban Life 4', subtitle: 'Subtitle 14', date: '' },
+  { id: 15, imgSrc: Img7, title: 'Urban Life 5', subtitle: 'Subtitle 15', date: '' },
+  { id: 16, imgSrc: Img8, title: 'Abstract 1', subtitle: 'Subtitle 16', date: '' },
+  { id: 17, imgSrc: Img10, title: 'Abstract 2', subtitle: 'Subtitle 17', date: '' },
+  { id: 18, imgSrc: Img11, title: 'Abstract 3', subtitle: 'Subtitle 18', date: '' },
+  { id: 19, imgSrc: Img4, title: 'Abstract 4', subtitle: 'Subtitle 19', date: '' },
+  { id: 20, imgSrc: Img5, title: 'Abstract 5', subtitle: 'Subtitle 20', date: '' },
+  { id: 21, imgSrc: Img6, title: 'Misc 1', subtitle: 'Subtitle 21', date: '' },
+  { id: 22, imgSrc: Img7, title: 'Misc 2', subtitle: 'Subtitle 22', date: '' },
+  { id: 23, imgSrc: Img8, title: 'Misc 3', subtitle: 'Subtitle 23', date: '' },
+];
+
+const sections = [
+  {
+    header: 'Nature',
+    subheader: 'The beauty of the natural world',
+    images: data.slice(0, 5)
+  },
+  {
+    header: 'Architecture',
+    subheader: 'Marvels of human engineering',
+    images: data.slice(5, 10)
+  },
+  {
+    header: 'Urban Life',
+    subheader: 'Scenes from bustling cityscapes',
+    images: data.slice(10, 15)
+  },
+  {
+    header: 'Abstract',
+    subheader: 'Artistic expressions and abstract views',
+    images: data.slice(15, 20)
+  },
+  {
+    header: 'Miscellaneous',
+    subheader: 'A mix of different subjects',
+    images: data.slice(20, 23)
+  },
+];
+
+export { data, sections };
--- src/pages/Gallery/GalleryPage.jsx
+++ src/pages/Gallery/GalleryPage.jsx
@@ -1,71 +1,17 @@
-// GalleryPage.jsx
-import React from 'react';
-import Img1 from '../../assets/gallery/img1.jpg';
-import Img2 from '../../assets/gallery/img2.jpg';
-import Img3 from '../../assets/gallery/img3.jpg';
-import Img4 from '../../assets/gallery/img4.jpg';
-import Img5 from '../../assets/gallery/img5.jpg';
-import Img6 from '../../assets/gallery/img6.jpg';
-import Img7 from '../../assets/gallery/img7.jpg';
-import Img8 from '../../assets/gallery/img8.jpg';
-import Img9 from '../../assets/gallery/img9.jpg';
-import Img10 from '../../assets/gallery/img10.jpg';
-import Img11 from '../../assets/gallery/img11.jpg';
-
-import { lazy, useEffect, useState } from "react";
-import './GalleryPage.css'
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
-import styled from 'styled-components';
+import React, { lazy, useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-const AnchorComponent = lazy(() => import("../../components/Anchor"));
-const SocialIcons = lazy(() => import("../../components/SocialIcons"));
-const HomeButton = lazy(() => import("../../components/HomeButton"));
-const LogoComponent = lazy(() => import("../../components/LogoComponent"));
-const BigTitle = lazy(() => import("../../components/BigTitle"));
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { sections } from '../../assets/data/GalleryData';
 
+import './GalleryPage.css';
 
-const GalleryWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-  gap: 2em;
-  padding: 10px;
-  background-color: #ece9e8;
-  opacity: 1;
-  background-image: radial-gradient(#08090a 1.1px, #ece9e8 1.1px);
-  background-size: 22px 22px;
-`;
-
-const Container = styled.div`
-  background-color: #ece9e8;
-  opacity: 1;
-  background-image: radial-gradient(#08090a 1.1px, #ece9e8 1.1px);
-  background-size: 22px 22px;
-  width: 100%;
-  height: auto;
-  position: relative;
-  padding-bottom: 5rem;
-`;
-
-const ImageBox = styled(motion.div)`
-  overflow: hidden;
-  border-radius: 15px;
-  cursor: pointer;
-  border: 4px solid #ece9e8;
-
-  img {
-    width: 100%;
-    height: auto;
-    display: block;
-    transition: transform 0.3s ease-in-out;
-
-    &:hover {
-      transform: scale(1.1);
-    }
-  }
-`;
+const AnchorComponent = lazy(() => import('../../components/Anchor'));
+const SocialIcons = lazy(() => import('../../components/SocialIcons'));
+const HomeButton = lazy(() => import('../../components/HomeButton'));
+const LogoComponent = lazy(() => import('../../components/LogoComponent'));
+const BigTitle = lazy(() => import('../../components/BigTitle'));
 
 const GalleryPage = () => {
-
   const [number, setNumber] = useState(0);
 
   useEffect(() => {
@@ -73,63 +19,52 @@ const GalleryPage = () => {
     setNumber(parseInt(num));
   }, []);
 
-
-  
-  const data = [
-    { id: 1, imgSrc: Img1, title: '', subtitle: '', date: '' },
-    { id: 2, imgSrc: Img2, title: '', subtitle: '', date: '' },
-    { id: 3, imgSrc: Img3, title: '', subtitle: '', date: '' },
-    { id: 4, imgSrc: Img4, title: '', subtitle: '', date: '' },
-    { id: 5, imgSrc: Img5, title: '', subtitle: '', date: '' },
-    { id: 6, imgSrc: Img6, title: '', subtitle: '', date: '' },
-    { id: 7, imgSrc: Img7, title: '', subtitle: '', date: '' },
-    { id: 8, imgSrc: Img8, title: '', subtitle: '', date: '' },
-    { id: 9, imgSrc: Img9, title: '', subtitle: '', date: '' },
-    { id: 10, imgSrc: Img10, title: '', subtitle: '', date: '' },
-    { id: 11, imgSrc: Img11, title: '', subtitle: '', date: '' },
-    { id: 12, imgSrc: Img4, title: '', subtitle: '', date: '' },
-    { id: 13, imgSrc: Img5, title: '', subtitle: '', date: '' },
-    { id: 14, imgSrc: Img6, title: '', subtitle: '', date: '' },
-    { id: 15, imgSrc: Img7, title: '', subtitle: '', date: '' },
-    { id: 16, imgSrc: Img8, title: '', subtitle: '', date: '' },
-    { id: 17, imgSrc: Img10, title: '', subtitle: '', date: '' },
-    { id: 18, imgSrc: Img11, title: '', subtitle: '', date: '' },
-    { id: 19, imgSrc: Img4, title: '', subtitle: '', date: '' },
-    { id: 20, imgSrc: Img5, title: '', subtitle: '', date: '' },
-    { id: 21, imgSrc: Img6, title: '', subtitle: '', date: '' },
-    { id: 22, imgSrc: Img7, title: '', subtitle: '', date: '' },
-    { id: 23, imgSrc: Img8, title: '', subtitle: '', date: '' },
-  ];
+  const imageVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
+    exit: { opacity: 0, scale: 0.8 }
+  };
 
   return (
     <>
-      <Container>
-          <LogoComponent />
-          <HomeButton />
-          <SocialIcons />
-          <AnchorComponent number={number} />
-          <BigTitle text="Posts" top="5rem" left="5rem" />
-        </Container>
-      <GalleryWrapper>
-        <ResponsiveMasonry
-                  columnsCountBreakPoints={{350: 1, 750: 3, 900: 6}}
-              >
-                  <Masonry gutter="2rem">
-        {data.map((item) => (
-          <ImageBox
-            key={item.id}
-            whileHover={{ scale: 1.05 }}
-            transition={{ type: 'spring', stiffness: 300 }}
-          >
-            <img src={item.imgSrc} alt={`Gallery Image ${item.id}`} />
-            {item.title}
-          </ImageBox>
+      <div className="container">
+        <LogoComponent />
+        <HomeButton />
+        <SocialIcons />
+        <AnchorComponent number={number} />
+        <BigTitle text="Gallery" left="25rem" top="15rem" />
+        {sections.map((section, index) => (
+          <div className="section-container" key={index}>
+            <h2 className='animate__animated animate__zoomIn animate__delay-1s'>{section.header}</h2>
+            <h3 className='animate__animated animate__backInUp animate__delay-1s'>{section.subheader}</h3>
+            <ResponsiveMasonry
+              columnsCountBreakPoints={{ 350: 1, 750: 3, 900: 4 }}
+            >
+              <Masonry gutter="20px">
+                {section.images.map((item) => (
+                  <motion.div
+                    className="image-box animate__animated animate__bounceInUp animate__delay-2s"
+                    key={item.id}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    variants={imageVariants}
+                    transition={{ duration: 0.5 }}
+                    whileHover={{ scale: 1 }}
+                  >
+                    <img src={item.imgSrc} alt={`Gallery Image ${item.id}`} loading="lazy" />
+                    <div className="image-info">
+                      <h4>{item.title}</h4>
+                      <p>{item.subtitle}</p>
+                    </div>
+                  </motion.div>
+                ))}
+              </Masonry>
+            </ResponsiveMasonry>
+          </div>
         ))}
-          </Masonry>
-          
-          </ResponsiveMasonry>
-        </GalleryWrapper>
-      </>
+      </div>
+    </>
   );
 };
 
