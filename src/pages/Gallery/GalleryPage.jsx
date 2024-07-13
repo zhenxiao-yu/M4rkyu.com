a84941c398a85bc63a2952f5b48cbd855685406e
Mark Yu <zyu347@uwo.ca>
2024-07-13 00:19:26 -0400

    added croll

--- src/pages/Gallery/GalleryPage.jsx
+++ src/pages/Gallery/GalleryPage.jsx
@@ -2,7 +2,6 @@ import React, { lazy, useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import { sections } from '../../assets/data/GalleryData';
-
 import './GalleryPage.css';
 
 const AnchorComponent = lazy(() => import('../../components/Anchor'));
@@ -25,6 +24,14 @@ const GalleryPage = () => {
     exit: { opacity: 0, scale: 0.8 }
   };
 
+  const handleScrollToSection = (event) => {
+    const sectionId = event.target.value;
+    const sectionElement = document.getElementById(sectionId);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -33,15 +40,28 @@ const GalleryPage = () => {
         <SocialIcons />
         <AnchorComponent number={number} />
         <BigTitle text="Gallery" left="25rem" top="15rem" />
+
+        {/* Dropdown for sections */}
+        <div className="dropdown-container">
+          <select onChange={handleScrollToSection}>
+            <option value="">Select a section...</option>
+            {sections.map((section, index) => (
+              <option key={index} value={`section-${index}`}>
+                {section.header}
+              </option>
+            ))}
+          </select>
+        </div>
+
         {sections.length === 0 ? (
           <div className="empty-message">
             <h2>No sections available in the gallery at the moment.</h2>
           </div>
         ) : (
           sections.map((section, index) => (
-            <div className="section-container" key={index}>
-              <h2 className='animate__animated animate__zoomIn animate__delay-1s'>{section.header}</h2>
-              <h3 className='animate__animated animate__backInUp animate__delay-1s'>{section.subheader}</h3>
+            <div className="section-container" id={`section-${index}`} key={index}>
+              <h2 className="animate__animated animate__zoomIn animate__delay-1s">{section.header}</h2>
+              <h3 className="animate__animated animate__backInUp animate__delay-1s">{section.subheader}</h3>
               {section.images.length === 0 ? (
                 <div className="empty-message">
                   <h3>Oops! No images available at the moment</h3>
