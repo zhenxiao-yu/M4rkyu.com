4bafede99d8f850d36d8055a98ce5a6658285419
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-10-15 20:21:14 -0400

    rgb

--- src/assets/data/GalleryData.js
+++ src/assets/data/GalleryData.js
@@ -7,38 +7,37 @@ const sections = [
   },
   {
     header: '<Black and White>',
-    subheader: 'A curated collection of black and white photographs capturing the essence of simplicity and depth through shades of gray. This collection is a tribute to the purity and sophistication of black and white photography, where every detail tells a story.',
+    subheader: 'Minimalist photographs that explore the power of contrast and simplicity.',
     images: []
   },
   {
     header: '<ChangChun>',
-    subheader: 'This collection offers a glimpse into the daily life and culture of my hometown through my lens in 2024. Born in Changchun but moving to Beijing at six, I am now revisiting at 23, capturing the essence of a place that holds my earliest memories.',
+    subheader: 'Visual narrative of daily life and culture in my hometown, ChangChun 2024.',
     images: []
   },
   {
     header: '<Shanghai>',
-    subheader: 'In June 2024, I embarked on a long-awaited journey to Shanghai with an old friend. This vibrant city, a symphony of old-world charm and modern marvels, unfolded before us with unforgettable experiences. Here is a collection of photos capturing our exploration through bustling streets, tranquil gardens, towering skyscrapers, and hidden gems. ',
+    subheader: 'Highlights from my June 2024 trip to Shanghai.',
     images: []
   },
   {
     header: '<Los Angeles>',
-    subheader: 'This collection chronicles my one-week adventure in Los Angeles after flying back from China in 2024. Each photo captures the quirks and charms of the city, from the laid-back vibes of Venice Beach to my attempts at hiking in Griffith Park. Join me as I navigate LA’s iconic spots, chase sunsets, and discover why traffic here really is as legendary as they say.',
+    subheader: 'Journey to the roof of the world, May 2024.',
     images: []
   },
    {
     header: '<Beijing>',
-    subheader: 'This collection captures my journey back to Beijing after five years away. Through my lens, I document the city\'s blend of old and new, from the quiet hutongs to the modern skyline. Each photo tells a story of rediscovery, capturing everyday life and the timeless spirit of a city that feels both familiar and new.',
+    subheader: 'A bittersweet return to Beijing, where the past and present quietly intertwine.',
     images: []
   },
   {
     header: '<Tibet>',
-    subheader: 'In May 2024, I embarked on a memorable road trip along Route 318 to visit the Mount Everest Base Camp. Despite experiencing slight altitude sickness, I captured stunning moments of my journey through Tibet. The breathtaking landscapes, ancient traditions, and resilient spirit of the region are vividly showcased in my photos.'
-,
+    subheader: 'Journey to the roof of the world, May 2024.',
     images: []
   },
   {
     header: '<Ontario>',
-    subheader: 'In Ontario, I completed my degree in software engineering at Western University in London, Ontario. This period was marked by intense academic pursuits, meaningful research, and internships that laid a strong foundation for my career. I also took part in various local events and explored the province\'s scenic landscapes, from the bustling cityscape of Toronto to the tranquil shores of the Great Lakes.',
+    subheader: 'From Toronto’s cityscape to Great Lakes’ tranquility.',
     images: []
   },
   
